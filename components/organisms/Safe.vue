0c484564cfc5f11239eb2826912d1b08a17770bc
André Luis dos Santos <alsss.ads@gmail.com>
2022-02-27 16:58:47 -0300

    Adiciona componente de cofres

--- components/organisms/Safe.vue
+++ components/organisms/Safe.vue
@@ -1,13 +1,32 @@
 <template>
-  <v-card>
-    <v-card-title>
-      {{ safe.name }}
-    </v-card-title>
+  <v-card dark>
+    <v-toolbar :color="safe.color">
+      <v-toolbar-title>{{safe.name}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-row align="center">
+      <v-col>
+        <v-card-text>{{safe.description}}</v-card-text>
+      </v-col>
+    </v-row>
+     <v-card-actions>
+      <v-card-text class="text-right blue--text text-lighten-3">{{total}}</v-card-text>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script setup>
+import {computed} from '@nuxtjs/composition-api'
+
 const props = defineProps({
   safe: Object
+})
+
+const total 
+= computed(() =>{
+  return `R$ ${props.safe.total}`
 })
 </script>
