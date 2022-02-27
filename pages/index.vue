d1b29e2cb13e9b3912add117d112e5a84b93847b
André Luis dos Santos <alsss.ads@gmail.com>
2022-02-27 16:59:02 -0300

    Adiciona cards de cofres

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,22 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="8">
+    <v-col cols="12" sm="8" md="11">
       <v-card>
-        <v-card-title>
+        <v-card-title class="title">
           Carteira virtual
         </v-card-title>
         <v-row align="center" class="mx-1">
-          <v-card-text>
-            Aqui possisvelmente vai ter a informação do total guardado
-          </v-card-text>
+          <v-col>
+            <v-card-text>
+              Aqui possisvelmente vai ter a informação do total guardado
+            </v-card-text>
+          </v-col>
+          <v-col class="text-right">
+            <v-card-text>Total:  <strong> {{total}} </strong></v-card-text>
+          </v-col>
         </v-row>
         <v-divider class="mx-1 mt-4"></v-divider>
-        <v-card-title>
+        <v-card-title class="subtitle-2">
           Cofres: 
         </v-card-title>
         <v-row align="center" class="px-2">
@@ -19,19 +24,23 @@
             <Safe :safe="item" />
           </v-col>
         </v-row>
+        <v-card-actions class="mt-5"></v-card-actions>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script setup>
-import {ref} from '@nuxtjs/composition-api'
+import {ref, computed} from '@nuxtjs/composition-api'
 
 import Safe from '@/components/organisms/Safe.vue'
 
 const safes = ref([
-  {name: 'Cofre 1'},
-  {name: 'Cofre 2'}
+  {name: 'Cofre 1', description: 'teste de descriçcão', total: 55.44},
+  {name: 'Cofre 2', description: 'teste de descriçcão', total: 55.44}
 ])
 
+const total = computed(() => {
+  return 'R$ 55.50'
+})
 </script>
